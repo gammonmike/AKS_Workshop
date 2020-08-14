75109552bfdf75128ac9f8092f075e8f8b284ed3
Mike Man Fung Mak <mikemak@gammonconstruction.com>
2020-08-14 10:34:09 +0800

    Add project files.

---
+++ AKS_Workshop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AKS_Workshop.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
