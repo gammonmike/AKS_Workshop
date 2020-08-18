547a2dfa8eda35945745171eb5b7129a8f03f14b
Mike Mak <mikemak@gammonconstruction.com>
2020-08-18 15:37:47 +0800

    update

--- AKS_Workshop/Controllers/GreetingController.cs
+++ AKS_Workshop/Controllers/GreetingController.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+
+
 namespace AKS_Workshop.Controllers
 {
   [Route("api/[controller]")]
@@ -14,6 +19,10 @@ namespace AKS_Workshop.Controllers
 
     public string Get(int id)
     {
+      TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+      configuration.InstrumentationKey = "61b7fb78-d207-411d-b7c5-59df48e5703f";
+      var telemetryClient = new TelemetryClient(configuration);
+      telemetryClient.TrackTrace("Greeting Controller Get Id: " + id);
       return "Hello World!";
     }
 
